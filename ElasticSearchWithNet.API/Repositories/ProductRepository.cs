9130bacb7c300c44d8daaf8339575333c923bb07
sedatbilece <seda1tbilece@gmail.com>
2023-07-08 13:31:42 +0300

    add ProductRepository

---
+++ ElasticSearchWithNet.API/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using ElasticSearchWithNet.API.Models;
+using Nest;
+
+namespace ElasticSearchWithNet.API.Repositories
+{
+    public class ProductRepository
+    {
+
+        private readonly ElasticClient _client;
+        public ProductRepository(ElasticClient client)
+        {
+            _client = client;
+        }
+
+
+        public async Task<Product> SaveAsync(Product newProduct)
+        {
+            newProduct.Created= DateTime.Now;
+
+            var response = await _client.IndexAsync(newProduct, x => x.Index("products"));
+            
+            //fast fail
+            if (!response.IsValid) return null;
+
+
+            newProduct.Id=response.Id;
+            return newProduct;
+
+        }
+    }
+}
