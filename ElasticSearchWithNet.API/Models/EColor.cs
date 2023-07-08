d4ffe4913ff5784f7f3e0214066d1a5bf28bd3c3
sedatbilece <seda1tbilece@gmail.com>
2023-07-08 12:16:43 +0300

    add Models

---
+++ ElasticSearchWithNet.API/Models/EColor.cs
@@ -0,0 +1,10 @@
+﻿namespace ElasticSearchWithNet.API.Models
+{
+    public enum EColor
+    {
+        Red=1,
+        Blue = 2,
+        Green=3
+
+    }
+}
---
+++ ElasticSearchWithNet.API/Models/ProductFeature.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace ElasticSearchWithNet.API.Models
+{
+    public class ProductFeature
+    {
+
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public EColor Color { get; set; }
+    }
+}
